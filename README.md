4bfc2a1d0d3568f5e8e4b59c6f6dde1f3ab04973
eddy <hyun11262000@gmail.com>
2022-09-13 21:24:06 +0900

    Example sum, max Implemented

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cs332-assign
+
+Assign for POSTECH [CSED332-01]
+
+Course Page: [SW Design][homepagelink]
+
+[homepagelink]: http://pl.postech.ac.kr/~gla/cs332/index.html
--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -2,6 +2,8 @@ package example
 
 import common._
 
+import scala.annotation.tailrec
+
 object Lists {
   /**
    * This method computes the sum of all elements in the list xs. There are
@@ -23,7 +25,14 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    @tailrec
+    def sumRecur(xs: List[Int], sumInt: Int): Int = {
+      if (xs.isEmpty) sumInt
+      else sumRecur(xs.tail, sumInt + xs.head)
+    }
+    sumRecur(xs, 0)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +47,16 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    @tailrec
+    def maxRecur(xs: List[Int], maxInt: Int): Int = {
+      if (xs.isEmpty) maxInt
+      else {
+        val newMaxInt = if (xs.head > maxInt) xs.head else maxInt
+        maxRecur(xs.tail, newMaxInt)
+      }
+    }
+    if (xs.isEmpty) throw new NoSuchElementException("Empty List")
+    else maxRecur(xs, -2147483648)
+  }
 }
