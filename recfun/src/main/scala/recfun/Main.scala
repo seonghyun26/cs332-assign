44e969b542bcecb7f5e2e62f18c2fd799cb0559b
eddy <hyun11262000@gmail.com>
2022-09-13 22:45:12 +0900

    AS2 #1 Pascal Implemented

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,15 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c < 0 || r < 0 || c > r) throw new Exception("c or r is out of range")
+    else{
+      if (c == 0 || c == r) 1 
+      else {
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
+      }
+    }
+  }
 
   /**
    * Exercise 2
