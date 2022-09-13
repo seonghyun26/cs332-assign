3e3e99c44efbe171a583183c4b69e764e5c88d81
eddy <hyun11262000@gmail.com>
2022-09-13 23:45:23 +0900

    AS2 #3 CountingChange Implemented

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -49,6 +49,8 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    money
+    if ( money < 0 || coins.isEmpty )  0
+    else if ( money == 0) 1
+    else countChange(money-coins.head, coins) + countChange(money, coins.tail)
   }
 }
