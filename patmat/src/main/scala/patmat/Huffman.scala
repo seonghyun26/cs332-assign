496a9ab87f1d09f3cd8ef0d3915ca8d23210e287
eddy <hyun11262000@gmail.com>
2022-09-22 13:54:52 +0900

    AS4 #1 Implementation - weight, chars

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -26,9 +26,19 @@ object Huffman {
 
   // Part 1: Basics
 
-  def weight(tree: CodeTree): Int = ??? // tree match ...
+  def weight(tree: CodeTree): Int = {
+    tree match {
+      case Fork(left, right, chars, weight) => weight
+      case Leaf(char, weight) => weight
+    }
+  } // tree match ...
 
-  def chars(tree: CodeTree): List[Char] = ??? // tree match ...
+  def chars(tree: CodeTree): List[Char] = {
+    tree match {
+      case Fork(left, right, chars, weight) => chars
+      case Leaf(char, weight) => List[Char](char)
+    }
+  } // tree match ...
 
   def makeCodeTree(left: CodeTree, right: CodeTree) =
     Fork(left, right, chars(left) ::: chars(right), weight(left) + weight(right))
