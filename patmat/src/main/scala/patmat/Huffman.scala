a231f991a8ed17e651b4513812345195d9746490
eddy <hyun11262000@gmail.com>
2022-09-29 20:07:45 +0900

    AS4 #2 Construction - singleton function, test case

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -100,7 +100,12 @@ object Huffman {
   /**
    * Checks whether the list `trees` contains only one single code tree.
    */
-  def singleton(trees: List[CodeTree]): Boolean = ???
+  def singleton(trees: List[CodeTree]): Boolean = {
+    trees match {
+      case _ :: Nil => true
+      case _ => false
+    }
+  }
 
   /**
    * The parameter `trees` of this function is a list of code trees ordered
