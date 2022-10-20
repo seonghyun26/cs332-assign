6894fa1038f100f868bb399db50b6f783c401568
eddy <hyun11262000@gmail.com>
2022-10-20 22:58:32 +0900

    AS6 #7 Solving the Game - Putting Things together

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -83,13 +83,13 @@ trait Solver extends GameDef {
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(List((startBlock, Nil)).toStream, Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart filter {blocknMove => done(blocknMove._1)}
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -99,5 +99,8 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = {
+    if (pathsToGoal.isEmpty)  Nil
+    else pathsToGoal.map{_._2.reverse}.head
+  }
 }
