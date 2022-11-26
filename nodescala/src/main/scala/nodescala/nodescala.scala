b17fc39e1ad1ce1253684799ea53f7496b1a1ccb
eddy <hyun11262000@gmail.com>
2022-11-26 12:53:15 +0900

    AS7 #3 An Synchronous HTTP Server - The HTTP Server

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,14 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (response.hasNext && token.nonCancelled)
+    {
+      val nextResponse = response.next()
+      exchange.write(nextResponse)
+    }
+    exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +48,24 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+
+    val cancelSubscription = Future.run() { cancellationToken =>
+      async {
+        while( cancellationToken.nonCancelled ){
+          val (request, exchange) = await { listener.nextRequest() }
+          async {
+            val response = handler(request)
+            respond(exchange, cancellationToken, response)
+          }
+        }
+      }
+    }
+    
+    Subscription(listenerSubscription, cancelSubscription)
+  }
 
 }
 
